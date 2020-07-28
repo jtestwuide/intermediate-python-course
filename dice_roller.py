1de2aeb8f1ae87551f0c7730e33a222bed39783a
jtestwuide <67339169+jtestwuide@users.noreply.github.com>
2020-07-27 19:23:21 -0500

    Now rolling two dice

--- dice_roller.py
+++ dice_roller.py
@@ -1,6 +1,15 @@
-def main():
-  print('You rolled a die')
-  print('I have successfully figured out version control ')
+import random
 
-if __name__== "__main__":
-  main()
+
+def main():
+    dice_rolls = 2
+    dice_sum = 0  # Value begins at zero
+    for i in range(0, dice_rolls):
+        roll = random.randint(1, 6)  # Function performed twice
+        dice_sum += roll  # Value of dice_sum is revised?
+        print(f'You rolled a {roll}')
+    print(f'You have rolled a total of {dice_sum}')
+
+
+if __name__ == "__main__":
+    main()
